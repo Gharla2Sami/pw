51bde19dfcc21be526179abdafa7119493d9f3db
Gharla2Sami <gharlasamuel@gmail.com>
2021-03-12 12:30:51 +0100

    fix

--- push.js
+++ push.js
@@ -7,14 +7,15 @@ let vapidkeys = {
 
   push.setVapidDetails('mailto:test@code.co.uk', vapidkeys.publicKey, vapidkeys.privateKey)
 
-  let sub = {"endpoint":
+  let sub = {
+    endpoint:
         "https://fcm.googleapis.com/fcm/send/fTf4fb0AXNM:APA91bFdjV2wJLgdK5-WrLc_MyyoHX9tHpK5H7W6X8_nhzsWHOvL77A1hVgwXRw8v7YoUQpuqq_WmF-MrbKGAQP9Tz17zeVAr9w8o2fpv35ceUx3_90FaTuQBQASjlgnfaELcP1hUCvx",
-    "expirationTime":null,
-    "keys":{
-        "p256dh":
-        "BG7TJl6W1qnMDvtXzTWKttZH5ncb5Gy6ZA9Z-Mk6Lxw5x0KvgIQo9ZKDu3jZc0x5iWjUlE4J-Xc-y65LHca-pLc",
-    "auth":"wnHq9XrRMLQ4r4EfvPIlUQ"
-    }
-};
+    expirationTime:null,
+    keys:{
+        p256dh:
+            "BG7TJl6W1qnMDvtXzTWKttZH5ncb5Gy6ZA9Z-Mk6Lxw5x0KvgIQo9ZKDu3jZc0x5iWjUlE4J-Xc-y65LHca-pLc",
+        auth: "wnHq9XrRMLQ4r4EfvPIlUQ"}
+    };
+
   push.sendNotification(sub, 'test message')
 
